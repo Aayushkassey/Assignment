5db3770864e92ea691bdd0a311c8eb8c71358d30
Aayushkassey <surump3@gmail.com>
2026-04-15 11:15:09 +0545

    Forget Pass

--- proj/accounts/urls.py
+++ proj/accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
+    path('forget-password/', views.forget_password_view, name='forget_password'),
 ]
